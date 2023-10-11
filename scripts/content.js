47826b706423e8e92893d8071a99c9aa366d5b34
Suzana <grdic.suzana@gmail.com>
2023-10-11 14:40:07 +0200

    implemented chatGPT API

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,6 @@
 console.log('content captainov log')
+const apiKey = "<<your key>>"
+const baseUrl = "https://api.openai.com/v1/chat/completions"
 
 window.addEventListener('hashchange', e => {
   console.log('URL hash changed', e)
@@ -14,7 +16,35 @@ window.addEventListener('hashchange', e => {
         messageText = messageText + messageDiv[i].innerText ;
       
     }
-console.log("Message Text: " + messageText);
+    console.log("Message Text: " + messageText);
+    const responseFunction = response => response.json()
+    fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            content: "Please generate a response to the following email : " + messageText, // TODO: limit message text
+            role: "user"
+          }
+        ],
+        model: "gpt-3.5-turbo",
+        temperature: 1,
+        max_tokens: 1000
+
+      })
+    }).then(responseFunction)
+      .then(jsonResponse=>{
+        console.log(jsonResponse)
+      })
+      .catch(error=> {
+        console.log(error)
+      })
+
+
   }
   else
   {console.log('nismo u poruci...')}
